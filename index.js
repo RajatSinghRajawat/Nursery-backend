2484070b5c7b382a336ecf7e03d2ec3cd14f80de
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-04-27 15:41:12 +0530

    backend

--- index.js
+++ index.js
@@ -20,7 +20,10 @@ const errorHandler = require("./src/middleware/errorMiddleware");
 const app = express();
 const port = process.env.PORT || 5008;
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 app.options("*", cors()); // 👈 VERY IMPORTANT
 app.use(express.json({ limit: "2mb" }));
