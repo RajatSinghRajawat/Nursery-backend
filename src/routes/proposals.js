9d97bea441913884623ebf886742f03a8ebf7234
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-04-30 12:19:32 +0530

    push

--- src/routes/proposals.js
+++ src/routes/proposals.js
@@ -1,11 +1,20 @@
 const router = require('express').Router();
 
-const { createProposal , getAllProposals , getProposalById } = require('../controllers/proposals');
+const {
+    createProposal,
+    getAllProposals,
+    getProposalById,
+    updateProposal,
+    deleteProposal
+} = require('../controllers/proposals');
 
 
 router.post('/createProposals', createProposal);
 router.get('/', getAllProposals);
 router.get('/getproposalById/:id', getProposalById);
+router.get('/:id', getProposalById);
+router.put('/:id', updateProposal);
+router.delete('/:id', deleteProposal);
 
 
 module.exports = router;
