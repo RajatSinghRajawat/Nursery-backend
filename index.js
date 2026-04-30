0af22f51495dfe6d7497f859488fac6fa2dc72ff
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-04-30 12:49:50 +0530

    add proposals route and update index

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 
 app.options("*", cors()); // 👈 VERY IMPORTANT
-app.use(express.json({ limit: "2mb" }));
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/uploads", express.static(path.join(__dirname, "public/Uploads")));
