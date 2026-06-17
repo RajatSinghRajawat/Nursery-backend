b0d9c4aff4c82c5fcb0234993451a46e60f3152d
Rajat Singh Rajawat <rajatsinghrajawatrajawat@gmail.com>
2026-06-17 17:25:02 +0530

    push

--- src/models/User.js
+++ src/models/User.js
@@ -50,7 +50,7 @@ const userSchema = new mongoose.Schema(
       street: { type: String, trim: true },
       city: { type: String, trim: true },
       state: { type: String, trim: true },
-      postalCode: { type: String,
+      postalCode: { type: String, trim: true },
       country: { type: String, trim: true },
     },
     createdAt: {
